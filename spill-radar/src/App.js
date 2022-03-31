c6ea51a28ed781fe4fc5eca046011ac63741194d
Yeseon02 <yk2384@nyu.edu>
2022-04-01 01:55:15 +0400

    cleaned up app.js oil spill working

--- spill-radar/src/App.js
+++ spill-radar/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
 import map from './PersianGulfMap.png';
 import './App.css';
 import React from 'react';
 import ReactDom from 'react-dom';
 import { useState } from "react";
-import styled from 'styled-components'
 
-
-const useMousePosition = () => {
+const useMousePosition = () => { //get mouse position in coordinates
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   React.useEffect(() => {
     const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
-    window.addEventListener("mousemove", setFromEvent);
+    window.addEventListener("mousedown", setFromEvent);
 
     return () => {
-      window.removeEventListener("mousemove", setFromEvent);
+      window.removeEventListener("mousedown", setFromEvent);
     };
   }, []);
-
+  console.log(position);
   return position;
 };
 
@@ -26,26 +24,34 @@ const useMousePosition = () => {
 const App = () => {
   const[counter, setCounter] = React.useState(18); //counter
   const interval = React.useRef(null);
-  //const [circles, setCircles] = useState([]);
   const position = useMousePosition();
   const [cursorX, setCursorX] = useState();
   const [cursorY, setCursorY] = useState();
 
-  window.addEventListener('mousemove', (e) => {
+  window.addEventListener('mousemove', (e) => { //tracking mouse movement
     setCursorX(e.pageX);
     setCursorY(e.pageY);
   })
 
-  React.useEffect(() => {
+  React.useEffect(() => { 
     return () => stopCounter();
   }, []);
 
-  const containerStyle = {
-    height: '100px', 
-    width: '100px',
+  const startCounter = () => { //increment counter
+    if(interval.current) return;
+    interval.current = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 100);
   };
 
-  const elementStyle = {
+  const stopCounter= () => { //stop enlarging size
+    if(interval.current) {
+      clearInterval(interval.current);
+      interval.current = null
+    }
+  };
+
+  const circleStyle = {
     margin: '5px',
     height: `${counter}px`,
     width: `${counter}px`,
@@ -57,76 +63,25 @@ const App = () => {
     top: cursorY - 0.5*counter + 'px'
   };
 
-  // const getCoords = (event) => { //doesnt work yet
-  //   var e = event.target;
-  //   var dim = e.getBoundingClientRect();
-  //   var x = event.clientX - dim.left;
-  //   var y = event.clientY - dim.top;
-  //   return [x, y];
-  // };
-
-  // const creatObj = (event) => { //doesnt work yet
-  //   let [x, y] = getCoords(event);
-  //   console.log(x);
-  //   console.log(y);
-  //   let newCircle = ( 
-  //     <cicle 
-  //       key={circles.length + 1} 
-  //       cx={x} 
-  //       cy={y}
-  //       r="10"
-  //       stroke="black"
-  //       strokeWidth="1"
-  //       fill="blue"
-  //       />
-  //     );
-  //     let allCircles = [...circles, newCircle];
-  //     setCircles(allCircles);
-  // }
-
-  const startCounter = () => { //increment counter
-    if(interval.current) return;
-    interval.current = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 100);
-  };
-
-  const stopCounter= () => {
-    if(interval.current) {
-      clearInterval(interval.current);
-      interval.current = null
-    }
-  };
-
   return (
     <div className="App">
-      <div
+      <div className="circle"
         onMouseDown={startCounter}
         onMouseUp={stopCounter}
         onMouseLeave={stopCounter} 
-        style = {elementStyle}
+        style = {circleStyle}
       >
       </div>
 
-      <button onClick={ 
-        fetch('http://localhost:5000/qubo?size=20&x=0&y=5')
-        .then(response => response.json())
+      <button onClick={ //fetching data from backend
+        fetch('http://127.0.0.1:5000/qubo?size=20&x=0&y=5', 
+        {mode: 'cors'})
+        .then(response => response.json()) //use data to move the ships
         .then(data => console.log(data))}></button>
         
-      {/*<div className="cursor"
-        style = {{
-          left: cursorX + 'px',
-          top: cursorY + 'px'
-        }}
-        > 
-      </div>*/
-      }
-      <div>
+      <div> 
         <img className="App-map" src={map} alt="map of persian gulf" />
       </div>
-
-
-      {/* <button onClick={sayHello}>Default</button>; */}
     </div>
   );
 }
