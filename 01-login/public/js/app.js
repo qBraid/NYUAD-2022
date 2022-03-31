c3c1d15771e7857b3c136782c8db7c0638ced6f8
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2022-03-31 05:43:23 +0000

    initial

---
+++ 01-login/public/js/app.js
@@ -0,0 +1,217 @@
+// The Auth0 client, initialized in configureClient()
+let auth0 = null;
+
+/**
+ * Starts the authentication flow
+ */
+const login = async (targetUrl) => {
+  try {
+    console.log("Logging in", targetUrl);
+
+    const options = {
+      redirect_uri: window.location.origin
+    };
+
+    if (targetUrl) {
+      options.appState = { targetUrl };
+    }
+
+    await auth0.loginWithRedirect(options);
+  } catch (err) {
+    console.log("Log in failed", err);
+  }
+};
+
+/**
+ * Executes the logout flow
+ */
+const logout = () => {
+  try {
+    console.log("Logging out");
+    auth0.logout({
+      returnTo: window.location.origin
+    });
+  } catch (err) {
+    console.log("Log out failed", err);
+  }
+};
+
+/**
+ * Retrieves the auth configuration from the server
+ */
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+/**
+ * Initializes the Auth0 client
+ */
+const configureClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  auth0 = await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId
+  });
+};
+
+/**
+ * Checks to see if the user is authenticated. If so, `fn` is executed. Otherwise, the user
+ * is prompted to log in
+ * @param {*} fn The function to execute if the user is logged in
+ */
+const requireAuth = async (fn, targetUrl) => {
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    return fn();
+  }
+
+  return login(targetUrl);
+};
+
+// Will run when page finishes loading
+window.onload = async () => {
+  await configureClient();
+
+  // If unable to parse the history hash, default to the root URL
+  if (!showContentFromUrl(window.location.pathname)) {
+    showContentFromUrl("/");
+    window.history.replaceState({ url: "/" }, {}, "/");
+  }
+
+  const bodyElement = document.getElementsByTagName("body")[0];
+
+  // Listen out for clicks on any hyperlink that navigates to a #/ URL
+  bodyElement.addEventListener("click", (e) => {
+    if (isRouteLink(e.target)) {
+      const url = e.target.getAttribute("href");
+
+      if (showContentFromUrl(url)) {
+        e.preventDefault();
+        window.history.pushState({ url }, {}, url);
+      }
+    }
+  });
+
+  const isAuthenticated = await auth0.isAuthenticated();
+
+  if (isAuthenticated) {
+    console.log("> User is authenticated");
+    window.history.replaceState({}, document.title, window.location.pathname);
+    updateUI();
+    return;
+  }
+
+  console.log("> User not authenticated");
+
+  const query = window.location.search;
+  const shouldParseResult = query.includes("code=") && query.includes("state=");
+
+  if (shouldParseResult) {
+    console.log("> Parsing redirect");
+    try {
+      const result = await auth0.handleRedirectCallback();
+
+      if (result.appState && result.appState.targetUrl) {
+        showContentFromUrl(result.appState.targetUrl);
+      }
+
+      console.log("Logged in!");
+    } catch (err) {
+      console.log("Error parsing redirect:", err);
+    }
+
+    window.history.replaceState({}, document.title, "/");
+  }
+
+  updateUI();
+};
+
+
+// maps
+
+var map = L.map('map').setView([24.47, 54.36], 12.5);
+
+
+var token = "pk.eyJ1IjoiaGFtemFib3Vkb3VjaGUiLCJhIjoiY2wxZGk1MHo3MGhucjNibnJ2bjl6NTZ4dCJ9.IJQwmeYtLKNG_Cl_IqjkXw"
+
+L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${token}`, {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'your.mapbox.access.token'
+}).addTo(map);
+
+var coords1 =   
+var coords2 = [24.46, 54.37]
+var coords3 = [24.45, 54.39]
+
+var marker1 = L.marker(coords1).addTo(map);
+var marker2 = L.marker(coords2).addTo(map);
+var marker2 = L.marker(coords3).addTo(map);
+
+
+L.Routing.control({
+  waypoints: [
+    L.latLng(coords1[0], coords1[1]),
+    L.latLng(coords2[0], coords2[1])
+  ],
+  color: "blue",
+  lineOptions: { styles: [{ color: '#242c81', weight: 2 }] },
+  draggableWaypoints: false,
+}).addTo(map);
+
+
+L.Routing.control({
+  waypoints: [
+    L.latLng(coords3[0], coords3[1]),
+    L.latLng(coords2[0], coords2[1])
+  ],
+  lineOptions: { styles: [{ color: '#242c81', weight: 2 }] },
+  draggableWaypoints: false,
+}).addTo(map);
+
+// fetch("http://localhost:3010/api/private").then((res) => res.json()).then((data) => { console.log(data) })
+
+const callApi = async () => {
+  try {
+
+    // Get the access token from the Auth0 client
+    const token = await auth0.getTokenSilently();
+
+    // Make the call to the API, setting the token
+    // in the Authorization header
+    const response = await fetch("http://localhost:3010/api/private", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    // Fetch the JSON result
+    const responseData = await response.json();
+
+    console.log(responseData)
+
+  } catch (e) {
+    // Display errors in the console
+    console.error(e);
+  }
+};
+
+// organise our code here
+
+// create a function to add a marker 
+
+// fetch marker coords from backend
+
+// finish work on authentication
+
+// add a function that draws paths between markers 
+
+// add a buntton in the ui that gets the location and console log
+
+// validate coodrinates (ad)
+
+
