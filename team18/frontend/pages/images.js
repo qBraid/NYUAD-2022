a8e7db62053f93e574e519516ce04af1d2daaced
Silvester Yu <ly1164@nyu.edu>
2022-04-01 11:25:08 +0400

    Delete images.js

--- team18/frontend/pages/images.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function Images(props) {
-    const {width, height} = props;
-    console.log("images here");
-    return <img src={"./placeHolder.png"} width={width} height={height} />
-};
